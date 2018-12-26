fee503fed97e8d4ec4e7aa825b3d667153e15876
Yves Dierick <ydie22@gmail.com>
2018-12-26 15:45:09 +0100

    Adding Shipping

---
+++ src/Shipping/OrderPlacedHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Messages;
+using NServiceBus;
+using NServiceBus.Logging;
+
+namespace Shipping
+{
+    public class OrderPlacedHandler :
+        IHandleMessages<OrderPlacedEvent>
+    {
+        static ILog log = LogManager.GetLogger<OrderPlacedHandler>();
+
+        public Task Handle(OrderPlacedEvent message, IMessageHandlerContext context)
+        {
+            log.Info($"Shipping has received OrderPlacedEvent, OrderId = {message.OrderId}. Packaging and shipping to customer");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Shipping/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Shipping
+{
+	using System.Threading.Tasks;
+	using NServiceBus;
+
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			Console.Title = "Shipping";
+
+			var endpointConfiguration = new EndpointConfiguration("Shipping");
+
+			var transport = endpointConfiguration.UseTransport<RabbitMQTransport>();
+			transport.UseConventionalRoutingTopology();
+			transport.ConnectionString("host=localhost");
+			endpointConfiguration.EnableInstallers();
+			var conventions = endpointConfiguration.Conventions();
+			conventions.DefiningCommandsAs(
+				type => type.Name.EndsWith("Command"));
+			conventions.DefiningEventsAs(
+				type => type.Name.EndsWith("Event"));
+
+			var endpointInstance = await Endpoint.Start(endpointConfiguration)
+				.ConfigureAwait(false);
+
+			await Console.Out.WriteLineAsync("Press Enter to exit.");
+			await Console.In.ReadLineAsync();
+
+			await endpointInstance.Stop()
+				.ConfigureAwait(false);
+		}
+	}
+}
