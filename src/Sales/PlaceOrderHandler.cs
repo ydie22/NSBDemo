73b609e3181bf05718dca480c45a6f50b0ca88dc
Yves Dierick <ydie22@gmail.com>
2018-12-26 16:02:04 +0100

    Transient or fatal exceptions

--- src/Sales/PlaceOrderHandler.cs
+++ src/Sales/PlaceOrderHandler.cs
@@ -1,40 +1,42 @@
-﻿using System.Threading.Tasks;
-using Messages;
-using NServiceBus;
-using NServiceBus.Logging;
-
-namespace Sales
+﻿namespace Sales
 {
-    public class PlaceOrderHandler :
-        IHandleMessages<PlaceOrderCommand>
-    {
-        static ILog log = LogManager.GetLogger<PlaceOrderHandler>();
+	using System.Threading.Tasks;
+	using Messages;
+	using NServiceBus;
+	using NServiceBus.Logging;
 
-        public Task Handle(PlaceOrderCommand message, IMessageHandlerContext context)
-        {
-            log.Info($"Received PlaceOrderCommand, OrderId = {message.OrderId}");
+	public class PlaceOrderHandler :
+		IHandleMessages<PlaceOrderCommand>
+	{
+		static ILog log = LogManager.GetLogger<PlaceOrderHandler>();
 
-            // This is normally where some business logic would occur
+		public Task Handle(PlaceOrderCommand message, IMessageHandlerContext context)
+		{
+			log.Info($"Received PlaceOrderCommand, OrderId = {message.OrderId}");
 
-            #region ThrowTransientException
-            // Uncomment to test throwing transient exceptions
-            //if (random.Next(0, 5) == 0)
-            //{
-            //    throw new Exception("Oops");
-            //}
-            #endregion
+			// This is normally where some business logic would occur
 
-            // Uncomment to test throwing fatal exceptions
-            //throw new Exception("BOOM");
+			#region ThrowTransientException
 
-            var orderPlaced = new OrderPlacedEvent
-            {
-                OrderId = message.OrderId
-            };
-            
-            log.Info($"Publishing OrderPlacedEvent, OrderId = {message.OrderId}");
-            
-            return context.Publish(orderPlaced);
-        }
-    }
+			// Uncomment to test throwing transient exceptions
+			//if (DateTime.Now.Ticks % 5 == 0)
+			//{
+			//	throw new Exception("Oops");
+			//}
+
+			#endregion
+
+			// Uncomment to test throwing fatal exceptions
+			//throw new Exception("BOOM");
+
+			var orderPlaced = new OrderPlacedEvent
+			{
+				OrderId = message.OrderId
+			};
+
+			log.Info($"Publishing OrderPlacedEvent, OrderId = {message.OrderId}");
+
+			return context.Publish(orderPlaced);
+		}
+	}
 }
